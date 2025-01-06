d052df7efb693b1322b1bc9faec495784aa1f1b3
Edgarm <edgar.martinez@canteradigital.mx>
2025-01-06 16:05:12 -0600

    Delete - delete unused import

--- src/pages/OutsideHome.tsx
+++ src/pages/OutsideHome.tsx
@@ -1,5 +1,4 @@
 import { OutsideLayout } from '@/components/outside-layout/OutsideLayout';
-import '@/components/outside-layout/outsideLayout.css';
 
 export const OutsideHome = () => {
   return <OutsideLayout />;
